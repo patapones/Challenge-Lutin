16428e227bb6d87b62021f520a9ec22a7275395a
Neron Kevin <neronkevin@gmail.com>
2022-12-05 13:40:46 +0100

    Challenge Lutin Vierge

---
+++ index.js
@@ -0,0 +1,29 @@
+// Objectif du Challenge
+
+/*
+Jupipo le lutin nain a mal exporté le fichier de productivité de sa team de lutins fabriquant des peluches Rick & Morty… Aidez le sinon il va finir rôtis à la broche. Le père Noël attend un fichier avec ce format
+*/
+
+const data = [
+  "Armédiel",
+  [33, 44, 23, 11],
+  "Jeanot",
+  [88, 42, 10, 50],
+  "Gustavolito",
+  [94, 33],
+  "Karl",
+  [48, 98, 77, 43, 57],
+  "Puliple",
+  [1, 32, 83, 24],
+  "Toniok",
+  [56, 73, 41],
+  "Blablou",
+  [19, 27, 35, 65, 75],
+  "Lolilop",
+  [838, 3],
+  "Henricko",
+  [2, 4, 5, 5, 2, 99],
+];
+
+// Résultat attendu: ce format
+// { nomDuLutin: nombreDePeluches }
